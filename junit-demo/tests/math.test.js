40665f1c1d499d2f5fb15c6f9fefb08d937d5e94
gautamdevgrovers <gautam.dev@unthinkable.co>
2025-12-05 15:03:39 +0530

    removed testings dirs

--- junit-demo/tests/math.test.js
+++
@@ -1,9 +0,0 @@
-const { add, sub } = require('../src/math');
-
-test('adds 2 + 3 = 5', () => {
-  expect(add(2,3)).toBe(5);
-});
-
-test('subtract 5 - 2 = 1 (intentional fail)', () => {
-  expect(sub(5,2)).toBe(1); // this is wrong; correct is 3 -> will fail
-});
